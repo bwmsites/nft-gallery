403fcc329b7f02a5171e9cb64d8264e79b3ca162
Bruno Silva <bwmsites@gmail.com>
2022-03-14 12:39:55 -0300

    removes unused artfacts

--- src/modules/solana/solana.controller.ts
+++ src/modules/solana/solana.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Query, UsePipes } from '@nestjs/common';
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
 import { SolanaService } from '@modules/solana/solana.service';
-import { CustomValidatorPipe } from '@common/pipes/custom-validator.pipe';
-import { getNftsByAddressSchema } from './schemas/get-nfts-by-address.schema';
+import { GetNftsByAddressQueryDTO } from '@modules/solana/dto/GetNftsByAddressQuery.dto';
 
 @Controller('solana/nfts')
 export class SolanaController {
     constructor(private _solanaService: SolanaService) {}
 
     @Get()
-    @UsePipes(new CustomValidatorPipe(getNftsByAddressSchema))
-    async getNftsByAddress(@Query('address') address): Promise<unknown> {
+    async getNftsByAddress(
+        @Query(
+            new ValidationPipe({
+                transform: true,
+                transformOptions: { enableImplicitConversion: true },
+                forbidNonWhitelisted: true,
+            }),
+        )
+        query: GetNftsByAddressQueryDTO,
+    ): Promise<unknown> {
+        const { address } = query;
         return this._solanaService.getNftsByAddress(address);
     }
 }
