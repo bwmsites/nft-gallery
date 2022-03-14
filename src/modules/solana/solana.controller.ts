8b1fb67229b5dddf030f6b8d83df350ac19be0d9
Bruno Silva <bwmsites@gmail.com>
2022-03-13 21:22:47 -0300

    creates request validation tier

--- src/modules/solana/solana.controller.ts
+++ src/modules/solana/solana.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UsePipes } from '@nestjs/common';
 import { SolanaService } from '@modules/solana/solana.service';
+import { CustomValidatorPipe } from '@common/pipes/custom-validator.pipe';
+import { getNftsByAddressSchema } from './schemas/get-nfts-by-address.schema';
 
 @Controller('solana/nfts')
 export class SolanaController {
     constructor(private _solanaService: SolanaService) {}
 
     @Get()
-    async getNftsByAddress(): Promise<unknown> {
-        // TODO: get token thru params
-        const address = 'GzYfWA3af4M29VdmDaCvUvyUnm9JkFZsybFoRg2J31qJ';
-
+    @UsePipes(new CustomValidatorPipe(getNftsByAddressSchema))
+    async getNftsByAddress(@Query('address') address): Promise<unknown> {
         return this._solanaService.getNftsByAddress(address);
     }
 }
