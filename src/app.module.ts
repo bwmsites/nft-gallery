f5fd2ad4f3779f44b8218a2ad61a56ed7c8bc89f
Bruno Silva <bwmsites@gmail.com>
2022-03-14 23:49:49 -0300

    adds httpModule for http requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppConfigModule } from '@config/app/config.module';
 import { HealthCheckModule } from '@modules/health-check/health-check.module';
 import { SolanaModule } from '@modules/solana/solana.module';
 import { SolanaService } from '@modules/solana/solana.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-    imports: [AppConfigModule, HealthCheckModule, SolanaModule],
+    imports: [AppConfigModule, HealthCheckModule, SolanaModule, HttpModule],
     controllers: [],
     providers: [SolanaService],
 })
