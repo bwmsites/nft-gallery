e760468a26301d49987ddbf5d6df13811cf823fb
Bruno Silva <bwmsites@gmail.com>
2022-03-14 23:55:32 -0300

    resolves solanaService dependencies

--- src/modules/solana/solana.service.spec.ts
+++ src/modules/solana/solana.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SolanaService } from '@modules/solana/solana.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('SolanaService', () => {
     let service: SolanaService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [HttpModule],
             providers: [SolanaService],
         }).compile();
 
