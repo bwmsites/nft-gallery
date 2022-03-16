6e92732fc629b8f716517b4e8d5b61e2a3898785
Bruno Silva <bwmsites@gmail.com>
2022-03-16 11:00:40 -0300

    implements rate limiting to api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { CacheModule, Module, Provider } from '@nestjs/common';
 import { AppConfigModule } from '@config/app/config.module';
 import { HealthCheckModule } from '@modules/health-check/health-check.module';
 import { SolanaModule } from '@modules/solana/solana.module';
 import { SolanaService } from '@modules/solana/solana.service';
 import { HttpModule } from '@nestjs/axios';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
+const ThrottlerProvider: Provider = {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+};
 
 @Module({
     imports: [
@@ -11,9 +18,13 @@ import { HttpModule } from '@nestjs/axios';
         HealthCheckModule,
         SolanaModule,
         HttpModule,
-        CacheModule.register({ ttl: 0 }),
+        CacheModule.register({ ttl: 120 }),
+        ThrottlerModule.forRoot({
+            ttl: 90,
+            limit: 10,
+        }),
     ],
     controllers: [],
-    providers: [SolanaService],
+    providers: [SolanaService, ThrottlerProvider],
 })
 export class AppModule {}
